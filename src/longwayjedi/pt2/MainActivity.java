5fe388d07ef813508bc73a02d15961c384a9a8dc
Foolkin <jeka321@yandex.ru>
2013-08-08 01:32:08 +0300

    video should work, but didnt work

--- src/longwayjedi/pt2/MainActivity.java
+++ src/longwayjedi/pt2/MainActivity.java
@@ -1,11 +1,21 @@
 package longwayjedi.pt2;
 
-import android.os.Bundle;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends Activity {
-
+	
+	VideoView screen;
+	
+	String urlYT = "rtsp://v5.cache8.c.youtube.com/CiILENy73wIaGQlv7bmyjXli4RMYDSANFEgGUgZ2aWRlb3MM/0/0/0/video.3gp";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +27,21 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	protected void playVideo() throws IllegalArgumentException, SecurityException, IllegalStateException, IOException  
+	{
+		screen = (VideoView)findViewById(R.id.screen);
+		screen.setVideoURI(Uri.parse(urlYT));
+		screen.setMediaController(new MediaController(this));
+		screen.requestFocus();
+		screen.start();
+		
+	}
+	
+	public void playClick(View view) throws IllegalArgumentException, SecurityException, IllegalStateException, IOException
+	{
+		playVideo();
 	}
 
 }
