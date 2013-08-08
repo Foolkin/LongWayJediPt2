8ff4ef3b1a4b0994ecca6a6db4623ea64caf6a21
Foolkin <jeka321@yandex.ru>
2013-08-08 18:44:41 +0300

    other implementation

--- src/longwayjedi/pt2/MainActivity.java
+++ src/longwayjedi/pt2/MainActivity.java
@@ -1,25 +1,28 @@
 package longwayjedi.pt2;
 
-import java.io.IOException;
-
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends Activity {
 	
-	VideoView screen;
+VideoView videoView;
 	
-	String urlYT = "rtsp://v5.cache8.c.youtube.com/CiILENy73wIaGQlv7bmyjXli4RMYDSANFEgGUgZ2aWRlb3MM/0/0/0/video.3gp";
+	String url = "http://www.androidbegin.com/tutorial/AndroidCommercial.3gp";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		videoView = (VideoView)findViewById(R.id.VideoView);
+		videoView.setVideoURI(Uri.parse(url));
+		videoView.setMediaController(new MediaController(this));
+		videoView.requestFocus();
+		videoView.start();
 	}
 
 	@Override
@@ -27,21 +30,6 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-	
-	protected void playVideo() throws IllegalArgumentException, SecurityException, IllegalStateException, IOException  
-	{
-		screen = (VideoView)findViewById(R.id.screen);
-		screen.setVideoURI(Uri.parse(urlYT));
-		screen.setMediaController(new MediaController(this));
-		screen.requestFocus();
-		screen.start();
-		
-	}
-	
-	public void playClick(View view) throws IllegalArgumentException, SecurityException, IllegalStateException, IOException
-	{
-		playVideo();
 	}
 
 }
