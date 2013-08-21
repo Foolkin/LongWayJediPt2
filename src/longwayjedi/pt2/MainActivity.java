40e1934afc952e5dde57071a25b8cf6a6c079218
Foolkin <jeka321@yandex.ru>
2013-08-21 13:03:20 +0300

    run video player activity implemented in main activity

--- src/longwayjedi/pt2/MainActivity.java
+++ src/longwayjedi/pt2/MainActivity.java
@@ -1,6 +1,9 @@
 package longwayjedi.pt2;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,18 +14,25 @@ public class MainActivity extends Activity {
 	
 VideoView videoView;
 	
-	String url = "http://www.androidbegin.com/tutorial/AndroidCommercial.3gp";
+//	String url = "http://www.androidbegin.com/tutorial/AndroidCommercial.3gp";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		videoView = (VideoView)findViewById(R.id.VideoView);
+		/*videoView = (VideoView)findViewById(R.id.VideoView);
 		videoView.setVideoURI(Uri.parse(url));
 		videoView.setMediaController(new MediaController(this));
+		videoView.setOnCompletionListener(new OnCompletionListener() {
+			public void onCompletion(MediaPlayer mp) {
+				videoView.start();
+			}
+		});
 		videoView.requestFocus();
-		videoView.start();
+		videoView.start();*/
+		
+		startActivity(new Intent(this, VideoPlayerActivity.class));
 	}
 
 	@Override
